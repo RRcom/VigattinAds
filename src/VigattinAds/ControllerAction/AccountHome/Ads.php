928a0d62d81205a6e62281976a28a04bd9d0ec3d
Resty <resty_rizal@live.com>
2013-12-27 17:05:13 +0800

    saving 12/27/2013D

--- src/VigattinAds/ControllerAction/AccountHome/Ads.php
+++ src/VigattinAds/ControllerAction/AccountHome/Ads.php
@@ -3,6 +3,9 @@ namespace VigattinAds\ControllerAction\AccountHome;
 
 use VigattinAds\Controller\AccountHomeController;
 use Zend\View\Model\ViewModel;
+use VigattinAds\DomainModel\UserManager;
+use VigattinAds\DomainModel\AdsUser;
+use Doctrine\ORM\Tools\Pagination;
 
 class Ads
 {
@@ -17,33 +20,33 @@ class Ads
     protected $viewModel;
 
     /**
-     * @var $user \VigattinAds\DomainModel\UserManager
+     * @var \VigattinAds\DomainModel\UserManager
      */
     protected $userManager;
+
+    /**
+     * @var \VigattinAds\DomainModel\AdsUser
+     */
+    protected $adsUser;
 
     public function __construct(AccountHomeController $accountHomeCtrl)
     {
         $this->accountHomeCtrl = $accountHomeCtrl;
         $this->viewModel = $accountHomeCtrl->getMainView();
         $this->userManager = $this->accountHomeCtrl->getServiceLocator()->get('VigattinAds\DomainModel\UserManager');
+        $this->adsUser = $this->userManager->getCurrentUser();
     }
 
     public function process()
     {
+        /** @var  $ads \Doctrine\Common\Collections\ArrayCollection */
+        $ads = $this->adsUser->get('ads');
         $actionContent = new ViewModel();
         $actionContent->setTemplate('vigattinads/view/account-home-ads');
-        $actionContent->setVariable('adsList', $this->getAdsList());
+        $actionContent->setVariable('adsList', $ads);
         $actionContent->setVariable('userManager', $this->userManager);
+        $actionContent->setVariable('adsUser', $this->adsUser);
         $this->viewModel->addChild($actionContent, 'actionContent');
         return $this->viewModel;
-    }
-
-    public function getAdsList()
-    {
-        /** @var $user \VigattinAds\DomainModel\AdsUser; */
-        $adsUser = $this->userManager->getCurrentUser();
-        /** @var $adsList \Doctrine\Common\Collections\ArrayCollection */
-        $adsList = $adsUser->get('ads');
-        return $adsList;
     }
 }
